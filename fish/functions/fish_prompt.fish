28da1510a887e296ba5a1682ddc68de28a83b5fb
Sundev79 <sundev79@nutdevs.org>
2022-02-22 19:02:02 +0100

    FISH: Updated fish_prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,12 +2,12 @@ function fish_prompt
     if not set -q VIRTUAL_ENV_DISABLE_PROMPT
         set -g VIRTUAL_ENV_DISABLE_PROMPT true
     end
-    set_color yellow
+    set_color yellow -o
     printf '%s' $USER
     set_color normal
     printf ' at '
 
-    set_color magenta
+    set_color brmagenta -o
     echo -n (prompt_hostname)
     set_color normal
     printf ' in '
@@ -16,11 +16,14 @@ function fish_prompt
     printf '%s' (prompt_pwd)
     set_color normal
 
-    # Line 2
-    echo
     if test -n "$VIRTUAL_ENV"
         printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
     end
-    printf '↪ '
+
+    set -g __fish_git_prompt_showupstream auto
+    printf (fish_git_prompt)
+
+    set_color brcyan -o
+    printf ' >>> '
     set_color normal
 end
