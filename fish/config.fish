07ad1c5c17088149e608fea360551b487f6b5b47
Sundev79 <sundev79@nutdevs.org>
2022-02-23 20:23:33 +0100

    FISH: Added git aliases

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,8 @@
 export PATH="$HOME/.cargo/bin:$PATH"
+
+alias gstat="git status"
+alias gsco="git commit"
+alias gspush="git push"
+alias gspull="git pull"
+alias gsupd="git fetch"
+alias gsadd="git add"
